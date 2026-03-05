1d54c527b73fd949f6b74c7f389caf0b50f965db
Diogo Soares Germano <diogogermano28@gmail.com>
2026-03-05 19:52:02 -0300

    add OrderedArrayDesc

--- arranjo-ordenado/src/main/java/org/example/Main.java
+++ arranjo-ordenado/src/main/java/org/example/Main.java
@@ -1,24 +1,42 @@
 package org.example;
 
+import org.example.array.OrderedArrayASC;
+import org.example.array.OrderedArrayDESC;
+
 public class Main {
 
     public static void main(String[] args) {
+        //1 TESTE ARRANJO ORDENADO DE FORMA CRESCENTE
 
         CemMilNumerosFactory numerosFactory = new CemMilNumerosFactory();
-        OrderedArray arranjo = new OrderedArray(100000);
+        OrderedArrayASC arranjoASC = new OrderedArrayASC(100000);
 
+        //GERADOR DE NUMEROS
         int[] numeros = numerosFactory.gerarCemMilNumeros();
 
+        //TEMPO INICIAL
         long T1 = System.nanoTime();
 
-        arranjo.inserir(numeros);
+        arranjoASC.inserirASC(numeros);
 
+        //TEMPO FINAL
         long T2 = System.nanoTime();
 
-        long tempoTotal = T2 - T1;
+        //T2-T1 PARA MÉDIA
+        long tempoTotalASC = T2 - T1;
 
-        double tempoMs = tempoTotal / 1_000_000.0;
+        //CONVERSÃO DE MÉDIA PARA MS
+        double tempoMs = tempoTotalASC/ 1_000_000.0;
 
         System.out.println("Tempo em ms: " + tempoMs);
+
+
+        //2 TESTE ARRANJO ORDENADO DE FORMA DECRESCENTE
+        OrderedArrayDESC arranjoDESC = new OrderedArrayDESC(100000);
+
+
+
+
+
     }
 }
